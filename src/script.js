29e3e7b922964e7e771f78dc46d9e03ac1b1f21b
Ninja Bear <109863675+light7dream@users.noreply.github.com>
2023-04-30 09:09:38 -0700

    update 3d particle model

--- src/script.js
+++ src/script.js
@@ -62,29 +62,61 @@ async function init() {
   const geometry = new THREE.TorusGeometry(.7, .2, 16, 100)
   const material = new THREE.PointsMaterial({ size: 0.005 })
 
-  mesh = (await loader.loadAsync("models/gltf/cube.glb")).scene.getObjectByName('mesh')
-  loader.load('models/gltf/cube.glb', (gltf)=>{
-    const Cube = gltf.scene.getObjectByName('Cube')
-    
-    Cube.material = new THREE.PointsMaterial({  size: 0.005,
-      color: 0xfffff });
-    Cube.position.set(0, 0, 0);
-    Cube.scale.set(.1, .1, .1);
-    const geometry = new THREE.BufferGeometry().setFromPoints(Cube.geometry.attributes.position);
-    const points = new THREE.Points(geometry, material);
+  loader.load('models/gltf/sphere.glb', (gltf)=>{
+    const mesh = gltf.scene.children[0];
+    const geometry = mesh.geometry.clone();
 
+    const positions = geometry.attributes.position.array;
+    const numParticles = positions.length / 3; // assuming each vertex has three components: x, y, z
+    const particlePositions = new Float32Array(numParticles * 3);
+
+    for (let i = 0; i < numParticles; i++) {
+      const i3 = i * 3;
+      const x = positions[i3 + 0];
+      const y = positions[i3 + 1];
+      const z = positions[i3 + 2];
+
+      // Calculate a random offset from the original vertex position
+      const offset = new THREE.Vector3(
+        Math.random() * 0.1 - 0.05,
+        Math.random() * 0.1 - 0.05,
+        Math.random() * 0.1 - 0.05
+      );
+
+      // Add the offset to the original vertex position to create a new particle position
+      const particlePosition = new THREE.Vector3(x, y, z).add(offset);
+
+      // Update the particle position buffer array
+      particlePositions[i3 + 0] = particlePosition.x;
+      particlePositions[i3 + 1] = particlePosition.y;
+      particlePositions[i3 + 2] = particlePosition.z;
+    }
+
+    // Create a new buffer attribute with the particle positions and set it on the geometry
+    geometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
+    const material = new THREE.PointsMaterial({
+      size: 0.01,
+      color: 0xffffff,
+      opacity: 0.8,
+      transparent: true
+    });
+    const points = new THREE.Points(geometry, material);
     scene.add(points);
+
+    points.material = material;
+    
+
+    // scene.add(gltf.scene);
   })
-  mesh = (await loader.loadAsync("models/gltf/cube.glb")).scene
-  const Cube = mesh.getObjectByName('Cube')
+
   // Mesh init
   sphere = new THREE.Points(geometry, material)
-
   scene.add(sphere)
-  scene.add(mesh)
   scene.add(pointLight)
   scene.add(camera)
 }
+
+
 function animate(){
 
   requestAnimationFrame( animate );
@@ -97,5 +129,6 @@ function animate(){
   }
   renderer.render(scene, camera)
 }
+
 init()
 animate()
